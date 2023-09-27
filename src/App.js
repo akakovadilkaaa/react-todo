de0fb030e530c242803f2290c2c46c3a5134d807
PC <salkynai.akakova@gmail.com>
2023-09-27 16:54:10 -0400

    add todoList Array

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
+var todoList = [{id: 1, title:'Complete asignment'},{id:2, title:'take an exam'},{id:3, title:'learn R langage'}];
 
 function App() {
   return (
     <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <ul>
+        {todoList.map((todo,i)=>{
+          return <div className='todo' style={{backgroundColor:'yellow',height:'40px',marginBottom:'10px'}}>
+            <h3>{todo.title}</h3>
+          </div>
+        })}
+      </ul>
     </div>
   );
 }
